17c5173fae53bbec03633aeaa6ff0a155c54c85d
worldofchristian <christianwaters@protonmail.com>
2023-10-23 21:28:22 -0600

    menu editor app

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import photocircle from '../img/photocircle.PNG'
 
 const Nav = () => {
@@ -8,14 +9,18 @@ const Nav = () => {
     className='flex items-center w-full py-4 fixed top-0 z-20 bg-slate-700 backdrop-filter bg-opacity-30 backdrop-blur-xl'>
         <div className='w-full flex justify-between items-center max-w-5xl mx-auto'>
             <div className='flex flex-row items-center gap-2 ml-4'>
+                <Link to='/'>
                 <img
                 src={photocircle}
                 alt='christian'
-                className='w-12 h-12 object-contain'
+                className='w-12 h-12 object-contain cursor-pointer'
                 />
+                </Link>
 
-                <p className='text-lg font-semibold'
+                <Link to='/'>
+                <p className='text-lg font-semibold cursor-pointer'
                 >Christian Waters</p>
+                </Link>
             </div>
 
             <button
@@ -27,4 +32,4 @@ const Nav = () => {
   )
 }
 
-export default Nav
+export default Nav;
--- src/components/Tavernstack.js
+++ src/components/Tavernstack.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useEffect } from 'react';
+import Nav from './Nav';
 
 const Tavernstack = () => {
   // load to the top of the page
@@ -9,11 +10,13 @@ const Tavernstack = () => {
       
   return (
     <>
-    <div className='flex items-center justify-center'>
+    <Nav />
+
+    <div className='flex items-center justify-center mt-16'>
       <div className='max-w-5xl'>
         <div
         className='flex-col my-12 p-2'>
-          <h1 className='text-4xl text-center font-bold text-base-content'
+          <h1 className='text-5xl text-center font-bold text-base-content'
           >Tavernstack</h1>
 
           <p className='text-lg font-base text-center mt-5 text-base-content'
@@ -25,30 +28,78 @@ const Tavernstack = () => {
           <p className='text-base font-base text-left mt-5 text-base-content'
           >This is the main feature and selling point of the application.<br /><br />
           This component allows the user to create, read, update, and delete their menu items.
-          These items are stored as documents in a FirestoreDB collection, each containing values such as name and price, 
-          as well as booleans like 'spicy' and 'vegan'. They also contain values for which category and subcategory the item belongs to.
-          Rather than directly editing trees in the database, the app sorts the items into their respective categories, and the user
-          edits these categories by changing the values assosiated with the item. This makes the CRUD actions more straightforward, while
-          minimizing the amount of access that's needed to the database.<br /><br />
-          
-          There's also a few children that take in props from the main component. These are simple pop-up 
-          modals that contain variables of either menu items, or menu categories. The variables appear in the main component next to an icon. 
-          When selected by the user, a handler sends the corresponding data to the child modal, sets the modal's state to active, and the input
-          fields within the modal are populated and can then be modified. A div with a slightly darkened background is placed underneath the modal
-          in order to focus the user's attention onto the content.</p>
+          </p>
+
+          <p className='text-base font-base text-left mt-5 text-base-content'
+          >These items are stored as documents in a FirestoreDB collection, each containing values such as name and price, 
+          as well as booleans like 'spicy' and 'vegan'. They also contain values for which category and subcategory the item belongs to. 
+          The app sorts the items into their respective categories, and the user edits these categories by changing the values assosiated 
+          with the item.
+          </p>
 
           <div className='flex items-center justify-center my-12'>
-            <div className="mockup-code">
-              <pre className='text-xs text-info'><code>{'<div'}</code></pre><pre className='text-xs text-success'><code>{'className'}</code></pre><pre className='text-xs'><code>{'="fixed inset-0 bg-black opacity-50 z-40"></div>'}</code></pre>
-              <pre className='text-xs text-info'><code>{'    '}{'<div'}</code></pre><pre className='text-xs text-success'><code>{'    '}{'className'}</code></pre><pre className='text-xs'><code>{'    '}{'=className="bg-white p-8 rounded-xl shadow-lg w-96"></div>'}</code></pre>
+            <div className='max-w-md'>
+              <div className="mockup-code">
+                <pre className='text-xs text-yellow-400'><code>{'['}</code></pre>
+
+                <pre className='text-xs text-blue-300'><code>{'    '}{'name:'}
+                <span className='text-orange-400'>{' '}{'"Original 16"'}</span></code></pre>
+
+                <pre className='text-xs text-blue-300'><code>{'    '}{'price:'}
+                <span className='text-orange-400'>{' '}{'"7"'}</span></code></pre>
+
+                <pre className='text-xs text-blue-300'><code>{'    '}{'description:'}
+                <span className='text-orange-400'>{' '}{'"16oz IPA"'}</span></code></pre>
+
+                <pre className='text-xs text-blue-300'><code>{'    '}{'category:'}
+                <span className='text-orange-400'>{' '}{'"Drinks"'}</span></code></pre>
+
+                <pre className='text-xs text-blue-300'><code>{'    '}{'subcategory:'}
+                <span className='text-orange-400'>{' '}{'"Draft"'}</span></code></pre>
+
+                <pre className='text-xs text-blue-300'><code>{'    '}{'gluten-free:'}
+                <span className='text-orange-400'>{' '}{'"false"'}</span></code></pre>
+
+                <pre className='text-xs text-blue-300'><code>{'    '}{'spicy:'}
+                <span className='text-orange-400'>{' '}{'"false"'}</span></code></pre>
+
+                <pre className='text-xs text-yellow-400'><code>{']'}</code></pre>
+              </div>
+            </div>
+          </div>
+
+          <p className='text-base font-base text-left mt-5 text-base-content'
+          >There's also a few children that take in props from the main component. These are simple pop-up 
+          modals that handle user input.
+          </p>
+
+          <p className='text-base font-base text-left mt-5 text-base-content'
+          >In the main component, each menu item and category has corresponding edit and delete icon. When selected by the user, a handler 
+          sends the selected data to the modal component, sets the modal state to active, and the input fields within the modal are populated and 
+          can then be modified. A div with a slightly darkened background is placed underneath the modal in order to focus the user's attention toward the content.
+          </p>
+
+          <div className='flex items-center justify-center my-12'>
+            <div className='max-w-md'>
+              <div className="mockup-code">
+                <pre className='text-xs text-blue-400'><code>
+                <span className='text-slate-400'>{'<'}</span>
+                {'div'}</code></pre>
+                <pre className='text-xs text-blue-300'><code>{'className='}
+                <span className='text-orange-400'>{'"fixed inset-0 bg-black opacity-50 z-40"'}
+                </span><span className='text-slate-400'>{'>'}</span>
+                <span className='text-blue-400'></span></code></pre>
+              </div>
             </div>
           </div>
 
           <p className='text-base font-base text-left mt-5 text-base-content'
           >The menu data is then displayed on the user's website in a similar manner to how the CRUD actions are performed. The data is sorted into categories
-          and displayed in a read-only format. If any of the booleans are true, a corresponding icon (such as a chili pepper) will be rendered next to that item.
+          and displayed in a read-only format.<br /><br />
+          
+          If any of the booleans are true, a corresponding icon (such as a chili pepper) will be rendered next to that item.
           Additionally, the categories themselves are implemented as accordion-style state variables. When active, the selected tab will turn blue. Only one category
-          is able to be active at any given time, so as to keep the UI clean and the user's attention focused on their current selection.</p>
+          is able to be active at any given time, so as to keep the UI clean.</p>
         </div>
       </div>
     </div>
