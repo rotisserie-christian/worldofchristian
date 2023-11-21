f7b326604bba892ffb9e801d0210244b4489d945
worldofchristian <102264428+worldofchristian@users.noreply.github.com>
2023-11-20 18:13:37 -0600

    removed img

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,8 +6,6 @@ import '@lottiefiles/lottie-player';
 import { FaAngleDoubleDown } from 'react-icons/fa';
 import { BiLogoReact, BiLogoTailwindCss, BiLogoFirebase } from 'react-icons/bi';
 import { FaNodeJs } from 'react-icons/fa';
-import screen from '../img/screen.png';
-import screenneronotes from '../img/screenneuronotes.png';
 
 const Home = () => {
   const scrollRef = useRef(null);
@@ -20,7 +18,7 @@ const Home = () => {
     <>
     <Nav />
 
-    <div className='flex items-center justify-center'>
+    <div className='flex flex-col items-center justify-center'>
       <div className='min-h-screen max-w-5xl'>
 
         {/* heading, links, animation, and scroll button*/}
@@ -65,87 +63,70 @@ const Home = () => {
           </div>
         </div>
 
-        <div className='flex-col'>
-          <div className='flex items-center justify-center'>
+        <div className='max-w-md'>
+          <div 
+          className='flex flex-col items-center justify-center'>
             <lottie-player
             src="https://lottie.host/37cf5e61-b065-4216-aba4-59c520754b4d/ZnsvbDRGJ0.json"
             background="transparent"
             speed="1.5"
-            style={{ width: '250px', height: '250px' }}
+            style={{ width: '300px', height: '300px' }}
             loop
             autoplay
             ></lottie-player>
-          </div>
-        </div>
 
-        <div className='max-w-md'>
-          <div 
-          className='bg-neutral-focus rounded-xl p-4 my-4'>
-            <div 
-            className='flex flex-col items-center justify-center'>
-              <p className='text-2xl font-bold'>
-              Highlights</p>
+            <div className='flex flex-row'>
+              <FaAngleRight className='text-xl mr-1 mt-1' />
 
-              <div className='flex flex-row mt-2'>
-                <FaAngleRight className='text-xl mr-1 mt-1' />
+              <p className='text-lg'
+              >Test-driven development - Jest</p>
+            </div>
 
-                <p className='text-lg'
-                >Test-driven development - Jest</p>
-              </div>
+            <div className='flex flex-row mt-2'>
+              <FaAngleRight className='text-xl mr-1 mt-1' />
 
-              <div className='flex flex-row mt-2'>
-                <FaAngleRight className='text-xl mr-1 mt-1' />
+              <p className='text-lg'
+              >Centralized state managment - Redux</p>
+            </div>
 
-                <p className='text-lg'
-                >Centralized state managment - Redux</p>
-              </div>
+            <div className='flex flex-row mt-2'>
+              <FaAngleRight className='text-xl mr-1 mt-1' />
 
-              <div className='flex flex-row mt-2'>
-                <FaAngleRight className='text-xl mr-1 mt-1' />
-
-                <p className='text-lg'
-                >Continuous deployment - Git/Netlify</p>
-              </div>
+              <p className='text-lg'
+              >Continuous deployment - Git/Netlify</p>
             </div>
           </div>
         </div>
 
-            <div className='flex items-center justify-center mb-40'>
-              <button
-              className='btn btn-neutral'
-              onClick={handleBtnClick}
-              >projects <FaAngleDoubleDown /></button>
-            </div>
-          </div>
+        <div className='flex items-center justify-center mt-12 mb-40'>
+          <button
+          className='btn btn-neutral'
+          onClick={handleBtnClick}
+          >projects <FaAngleDoubleDown /></button>
         </div>
+      </div>
 
-        {/* projects */}
-        <div>
-          {/* first two */}
+      {/* projects */}
+      <div>
+        <div 
+        className='flex item-center justify-center mt-40 mb-12'
+        ref={scrollRef}
+        >
           <div 
-          className='flex item-center justify-center my-12'
-          ref={scrollRef}
-          >
+          className='grid grid-cols-1 md:grid-cols-2 gap-6 mt-12 w-full max-w-5xl'>
             <div 
-            className='grid grid-cols-1 md:grid-cols-2 gap-6 mt-12 w-full max-w-5xl'>
+            className='bg-neutral-focus rounded-xl p-4'>
               <div 
-              className='bg-neutral-focus rounded-xl p-4'>
+              className='flex flex-col items-center justify-center'>
+                <h2 className='text-4xl font-bold mt-2'
+                >Tavernstack</h2>
+
+                <p className='text-lg font-base text-center mt-4'
+                >Website managment app for bars</p>
+                
                 <div 
-                className='flex flex-col items-center justify-center'>
-                  <h2 className='text-4xl font-bold mt-2'
-                  >Tavernstack</h2>
-
-                  <p className='text-lg font-base text-center mt-4'
-                  >Website managment app for bars</p>
-                  
-                  <div 
-                  className='flex flex-row mt-4'>
-                    <img
-                    src={screen}
-                    alt='app screenshot'
-                    className='max-w-[50%]'
-                    />
-
+                className='flex flex-row mt-4 mb-8'>
+                  <div className='flex flex-row gap-8'>
                     <div 
                     className='flex flex-col'>
                       <div className='flex flex-row'>
@@ -175,70 +156,68 @@ const Home = () => {
                         <p className='text-lg font-base text-left mt-5'
                         >Firebase</p>
                       </div>
-                      
+                    </div>
+
+                    <div className='flex flex-col'>
                       <a
-                      href='https://demo.tavernstack.com'
-                      alt='link to demo'
-                      target='_blank'
-                      rel="noreferrer"
-                      >
-                        <button
-                        className='btn btn-neutral mt-20'
-                        >live demo <FaAngleDoubleRight /></button>
-                      </a>
-                      
-                      <Link to='/tavernstack'>
-                        <button
-                        className='btn btn-neutral mt-2'
-                        >read more <FaAngleDoubleRight /></button>
-                      </Link>
+                        href='https://demo.tavernstack.com'
+                        alt='link to demo'
+                        target='_blank'
+                        rel="noreferrer"
+                        >
+                          <button
+                          className='btn btn-neutral mt-20'
+                          >live demo <FaAngleDoubleRight /></button>
+                        </a>
+                        
+                        <Link to='/tavernstack'>
+                          <button
+                          className='btn btn-neutral mt-2'
+                          >read more <FaAngleDoubleRight /></button>
+                        </Link>
                     </div>
                   </div>
                 </div>
               </div>
+            </div>
 
+            <div 
+            className='bg-neutral-focus rounded-xl p-4'>
               <div 
-              className='bg-neutral-focus rounded-xl p-4'>
-                <div 
-                className='flex flex-col items-center justify-center'>
-                  <h2 className='text-4xl font-bold mt-2'
-                  >Neuronotes</h2>
+              className='flex flex-col items-center justify-center'>
+                <h2 className='text-4xl font-bold mt-2'
+                >Neuronotes</h2>
 
-                  <p className='text-lg font-base text-center mt-4'
-                  >Learning resource for psychology & neuroscience</p>
+                <p className='text-lg font-base text-center mt-4'
+                >Learning resource for psychology & neuroscience</p>
+                
+                <div className='flex flex-row mt-4 mb-8 gap-8'>
                   
-                  <div 
-                  className='flex flex-row mt-4'>
-                    <img
-                    src={screenneronotes}
-                    alt='app screenshot'
-                    className='max-w-[50%]'
-                    />
+                  <div className='flex-col'>
+                    <div className='flex flex-row'>
+                      <BiLogoReact className='text-2xl mt-12 mr-2'/>
+                      
+                      <p className='text-lg font-base text-left mt-11'
+                      >React</p>
+                    </div>
 
-                    <div 
-                    className='flex flex-col'>
-                      <div className='flex flex-row'>
-                        <BiLogoReact className='text-2xl mt-12 mr-2'/>
-                        
-                        <p className='text-lg font-base text-left mt-11'
-                        >React</p>
-                      </div>
+                    <div className='flex flex-row'>
+                      <BiLogoTailwindCss className='text-2xl mt-6 mr-2'/>
+                      
+                      <p className='text-lg font-base text-left mt-6'
+                      >Tailwind</p>
+                    </div>
+                  </div>
 
-                      <div className='flex flex-row'>
-                        <BiLogoTailwindCss className='text-2xl mt-6 mr-2'/>
-                        
-                        <p className='text-lg font-base text-left mt-6'
-                        >Tailwind</p>
-                      </div>
-
-                      <a
+                  <div className='flex flex-col'>
+                    <a
                       href='https://neuronotes.netlify.app'
                       alt='link to neuronotes'
                       target='_blank'
                       rel="noreferrer"
                       >
                         <button
-                        className='btn btn-neutral mt-28'
+                        className='btn btn-neutral mt-10'
                         >live site <FaAngleDoubleRight /></button>
                       </a>
 
@@ -247,14 +226,16 @@ const Home = () => {
                         className='btn btn-neutral mt-2'
                         >read more <FaAngleDoubleRight /></button>
                       </Link>
-                    </div>
                   </div>
                 </div>
+
               </div>
             </div>
+
           </div>
-              
         </div>
+      </div>
+    </div>
     </>
   );
 };
